b0cce9d76c9ee43f0f4593ea986969940a3387a9
XiaoweiChen2597 <xc2597@columbia.edu>
2022-11-14 13:57:55 -0500

    update teammate

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -3,16 +3,83 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Team } from './team/team';
 import { MessageService } from "./message.service";
 import { catchError, throwError, Observable, of} from 'rxjs';
+import {AccountService} from "./account.service";
 
 @Injectable({
     providedIn: 'root'
   })
 
 export class TeamService {
+  teamurl = "http://127.0.0.1:5011/team/";
+  isLoggedIn = false;
+  notLoggedIn = true;
+  user = {
+    "uni": "",
+    "email": "",
+    "first_name": "",
+    "last_name": "",
+    "timezone": "N/A",
+    "major":"N/A",
+    "gender":"N/A",
+    "msg":"N/A",
+    "department": "N/A",
+  }
+  FEMALE_IMAGE = "../../assets/female.png";
+  MALE_IMAGE = "../../assets/male.png";
+  UNKNOWN_IMAGE = "../../assets/unknown.png";
+  profile_img = this.UNKNOWN_IMAGE;
+
+  header = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+    'key': 'x-api-key',
+    'value': 'NNctr6Tjrw9794gFXf3fi6zWBZ78j6Gv3UCb3y0x',
+  });
     constructor(
       private http: HttpClient,
-      private messageService: MessageService
+      private messageService: MessageService,
+      private accountService: AccountService
     ) {}
+  
+  ngOnInit(): void{
+    this.isLoggedIn = this.accountService.isLoggedIn;
+    this.notLoggedIn = !this.isLoggedIn;
+    if(this.isLoggedIn) {
+      let account = this.accountService.currentUser;
+      console.log(account);
+      this.user.uni = account.uni;
+      this.user.email = account.email;
+      this.user.first_name = account.first_name;
+      this.user.last_name = account.last_name;
+
+      // get profile
+      this.accountService.getProfile().subscribe(
+        (profile) => {
+          console.log(profile)
+          if(profile) {
+            this.user.timezone = profile.timezone;
+            this.user.gender = profile.gender;
+            this.user.major = profile.major;
+            this.user.msg = profile.personal_message;
+            if(this.user.gender == "f") {
+              this.profile_img = this.FEMALE_IMAGE;
+            } else if (this.user.gender == "m") {
+              this.profile_img = this.MALE_IMAGE;
+            } else {
+              this.profile_img = this.UNKNOWN_IMAGE;
+            }
+          }
+        }
+      )
+    } else {
+      this.isLoggedIn = false;
+      this.notLoggedIn = true;
+    }
+  }
+
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
@@ -52,6 +119,16 @@ export class TeamService {
     return of(result as T);
   };
   }
+  private setHeaders(): Headers {
+    const headersConfig = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    };
+
+    return new Headers(headersConfig);
+  }
+
 
   getTeamServiceUrl(): string {
       const theUrl = window.location.href;
@@ -65,9 +142,18 @@ export class TeamService {
 
   retrieve_all_team_by_params(params: any): Observable<any> {
     let teamUrl: string = "";
+    let headers =  new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+      'key': 'x-api-key',
+      'value': 'NNctr6Tjrw9794gFXf3fi6zWBZ78j6Gv3UCb3y0x'})
     if (params[`course_id`]) {
       teamUrl = this.getTeamServiceUrl() + `team/course_id=${params[`course_id`]}/limit=${params[`size`]}&offset=${params[`size`]*params[`page`]}`;
     }
+    const httpOptions = {headers: headers};
     console.log(teamUrl);
     return this.http.get(teamUrl).pipe(catchError(this.handleError<any>("SearchTeam")));
     }
